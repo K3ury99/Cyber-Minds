34bab69d728fdc8c45712c53d02d165306e8450f
K3ury <keury2004@hotmail.com>
2024-08-04 15:10:32 -0400

    Project: DB updated

--- CyberMinds/CyberMinds.Web/Models/CyberMindsContext.cs
+++ CyberMinds/CyberMinds.Web/Models/CyberMindsContext.cs
@@ -25,7 +25,7 @@ public partial class CyberMindsContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=LAPTOP-KORTEX\\SQL2022; database=CyberMinds; integrated security=true; TrustServerCertificate=Yes");
+        => optionsBuilder.UseSqlServer("");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
